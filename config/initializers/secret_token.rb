0d441cdafb675128c7d3a9344cb828a10cd390ec
vagrant <vagrant@precise64.(none)>
2012-07-27 10:29:55 +0000

    Setup for production

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hangover::Application.config.secret_token = '1ec0696a4cc96e2d09a7c910e2d099651a559da1c8cb842b1bd968f2c3a484101fed164040e3cc5d849034cd89f48835077ad1b4a8a0ed7a23965436746bc2e4'
